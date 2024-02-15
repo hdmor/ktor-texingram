3f51787570a2c5dd67fd935cf928887f3f521627
hdmor <hdmor.h@gmail.com>
2024-02-15 15:59:31 +0330

    Create room and members

---
+++ src/main/kotlin/ir/texingram/room/MemberAlreadyExistsException.kt
@@ -0,0 +1,3 @@
+package ir.texingram.room
+
+class MemberAlreadyExistsException : Exception("There is already a member with that username in the room!")
---
+++ src/main/kotlin/ir/texingram/room/RoomController.kt
@@ -0,0 +1,37 @@
+package ir.texingram.room
+
+import io.ktor.websocket.Frame
+import io.ktor.websocket.WebSocketSession
+import io.ktor.websocket.close
+import ir.texingram.data.MessageDataSource
+import ir.texingram.data.model.Message
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import java.util.concurrent.ConcurrentHashMap
+
+class RoomController(private val messageDataSource: MessageDataSource) {
+
+    private val members = ConcurrentHashMap<String, Member>()
+
+    fun onJoin(username: String, sessionId: String, socket: WebSocketSession) {
+        if (members.containsKey(username)) throw MemberAlreadyExistsException()
+        members[username] = Member(username = username, sessionId = sessionId, socket = socket)
+    }
+
+    suspend fun sendMessage(senderUsername: String, message: String) {
+        members.values.forEach { member ->
+            val messageEntity = Message(username = senderUsername, message = message, timestamp = System.currentTimeMillis())
+            messageDataSource.insert(messageEntity)
+
+            val parseMessage = Json.encodeToString(messageEntity)
+            member.socket.send(Frame.Text(parseMessage))
+        }
+    }
+
+    suspend fun getAllMessages(): List<Message> = messageDataSource.getAll()
+
+    suspend fun tryToDisconnect(username: String) {
+        members[username]?.socket?.close()
+        if (members.containsKey(username)) members.remove(username)
+    }
+}
